f0b39345ae11d35795caad28e122d1420b3e366a
Augustogany <auguss24@gmail.com>
2022-04-24 13:03:09 -0400

    refactorizando uses en seeder

--- database/seeds/TipoPagoTableSeeder.php
+++ database/seeds/TipoPagoTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\TipoPago;
 class TipoPagoTableSeeder extends Seeder
 {
     /**
@@ -11,7 +11,7 @@ class TipoPagoTableSeeder extends Seeder
      */
     public function run()
     {
-        \App\TipoPago::create([
+        TipoPago::create([
             'user_id' => 1,
             'sucursal_id' => 1,
             'nombrepago' => 'ATRASADOS',
