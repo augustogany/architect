416730954870cab349a2e5b68f43cd91c8547a78
Augustogany <auguss24@gmail.com>
2023-04-20 23:38:30 -0400

    Se agrego la suma de todas las columnas,fondo al heading y logo

--- app/Exports/IngresosMensuales.php
+++ app/Exports/IngresosMensuales.php
@@ -2,6 +2,8 @@
 
 namespace App\Exports;
 
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
@@ -9,6 +11,7 @@ use Maatwebsite\Excel\Concerns\WithCustomStartCell;
 use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\WithTitle;
 use Maatwebsite\Excel\Events\AfterSheet;
+use Maatwebsite\Excel\Events\BeforeSheet;
 use Carbon\Carbon;
 use \DB;
 
@@ -20,7 +23,54 @@ ShouldAutoSize,WithCustomStartCell,WithTitle,WithEvents
     */
     public function collection()
     {
-        return collect(DB::select('CALL ingresos_mensuales()'));
+        $data = collect(DB::select('CALL ingresos_mensuales()'));
+        // Calcular la suma de cada columna
+        $total_score = $data->sum('1');
+        $total_column3 = $data->sum('2');
+        $total_column4 = $data->sum('3');
+        $total_column5 = $data->sum('4');
+        $total_column6 = $data->sum('5');
+        $total_column7 = $data->sum('6');
+        $total_column8 = $data->sum('7');
+        $total_column9 = $data->sum('8');
+        $total_column10 = $data->sum('9');
+        $total_column11 = $data->sum('10');
+        $total_column12 = $data->sum('11');
+        $total_column13 = $data->sum('12');
+        $total_column14 = $data->sum('13');
+        $total_column15 = $data->sum('14');
+        $total_column16 = $data->sum('15');
+        $total_column17 = $data->sum('16');
+        $total_column18 = $data->sum('17');
+        $total_column19 = $data->sum('18');
+        $total_column20 = $data->sum('19');
+        $total_column21 = $data->sum('20');
+        $total_column22 = $data->sum('21');
+        $total_column23 = $data->sum('22');
+        $total_column24 = $data->sum('23');
+        $total_column25 = $data->sum('24');
+        $total_column26 = $data->sum('25');
+        $total_column27 = $data->sum('26');
+        $total_column28 = $data->sum('27');
+        $total_column29 = $data->sum('28');
+        $total_column30 = $data->sum('29');
+        $total_column31 = $data->sum('30');
+        $total_column32 = $data->sum('31');
+        $total_column33 = $data->sum('Total');
+        // Crear una nueva fila con los totales de cada columna
+        $total_row = ['Total Ingresos',$total_score, $total_column3, $total_column4, 
+            $total_column5, $total_column6,$total_column7, $total_column8
+            ,$total_column9, $total_column10, $total_column11, $total_column12
+            ,$total_column13,$total_column14,$total_column15,$total_column16,$total_column17,$total_column18
+            ,$total_column19,$total_column20,$total_column21,$total_column22,$total_column23,$total_column24
+            ,$total_column25,$total_column26,$total_column27,$total_column28,$total_column29
+            ,$total_column30,$total_column31,$total_column32,$total_column33
+        ];
+
+        // Agregar la nueva fila al final de la colección de datos
+        $data->push($total_row);
+
+        return $data;
     }
     public function headings(): array
     {
@@ -83,6 +133,7 @@ ShouldAutoSize,WithCustomStartCell,WithTitle,WithEvents
                 ['D5:S5',
                 'A2:C2' // Fila intermedia
                 ]);
+                $event->sheet->getStyle('A8:AG8')->applyFromArray($this->getStyleArray());
                 $mesActual = Carbon::now()->monthName;
                 $anioActual = Carbon::now()->year;
                 $event->sheet->setCellValue('A2', 'COLEGIO DE ARQUITECTOS DEL BENI');
@@ -110,5 +161,33 @@ ShouldAutoSize,WithCustomStartCell,WithTitle,WithEvents
     public function title(): string
     {
         return 'INGRESO MES';
+    }
+
+    protected function getStyleArray(): array
+    {
+        $styleArray = [
+            'font' => [
+                'bold' => true,
+                'size' => 13,
+                'color' => ['argb' => 'FFFDFD'],
+            ],
+            'fill' => [
+                'fillType' => Fill::FILL_SOLID,
+                'color' => ['argb' => 'ABA5A5'],
+            ],
+            'alignment' => [
+                'horizontal' => Alignment::HORIZONTAL_CENTER,
+                'vertical' => Alignment::VERTICAL_CENTER,
+            ],
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                    'color' => ['argb' => '8A8888'],
+                ],
+            ],
+            'wrapText' => true,
+        ];
+
+        return $styleArray;
     }
 }
